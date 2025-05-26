408fdb971640223ad93c93c214a13ee117dae012
eduardonk9999 <thiago.novak7@gmail.com>
2025-05-26 19:34:07 -0300

    init project

---
+++ src/main/java/org/example/Janela.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Janela extends JFrame {
+    private JPanel tela;
+
+    public Janela() {
+        tela = new JPanel() {
+            @Override
+            public void paintComponent(Graphics g) {
+
+            }
+        };
+
+        super.getContentPane().add(tela);
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new Janela();
+    }
+}
